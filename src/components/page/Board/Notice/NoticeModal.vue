ad2eb9faf75b17ab164cff0ddd860ecdd090a621
jsmin6330 <jsmin6330@gmail.com>
2024-12-17 15:53:44 +0900

    #5 Modal 이미지 파일첨부 기능 구현

--- src/components/page/Board/Notice/NoticeModal.vue
+++ src/components/page/Board/Notice/NoticeModal.vue
@@ -7,8 +7,19 @@
                     내용 :
                     <input type="text" v-model="noticeDetail.content"/>
                 </label>
-                파일 :<input type="file" style="display: none" id="fileInput" />
+                파일 :<input type="file" style="display: none" id="fileInput" @change="handlerFile"/>
                 <label class="img-label" htmlFor="fileInput"> 파일 첨부하기 </label>
+                <div>
+                    <div v-if="imageUrl" @click="fileDownload">
+                        <label>미리보기</label>
+                        <img :src = "imageUrl"/>
+                    </div>
+                    <div v-else>
+                    </div>
+                </div>
+                
+
+
                 <div>
                     <label>파일명</label>
                 </div>
@@ -27,6 +38,7 @@ import axios from 'axios';
 import { useModalStore } from '../../../../stores/modalState';
 import { useUserInfo } from '../../../../stores/userInfo';
 import { onMounted, onUnmounted } from 'vue';
+import { fi } from 'date-fns/locale';
 
 const emit = defineEmits(['postSuccess','modalClose']);
 const props = defineProps(['idx']);
@@ -34,6 +46,8 @@ const props = defineProps(['idx']);
 const modalState = useModalStore();
 const userInfo = useUserInfo();
 const noticeDetail = ref({});
+const imageUrl = ref('');
+const fileData = ref('');
 
 const handlerModal = (idx) => {
     // console.log(idx);
@@ -46,7 +60,15 @@ const hadlerSaveBtn = () => {
         loginId: userInfo.user.loginId,
         context: noticeDetail.value.content
     };
-    axios.post('/api/board/noticeSaveBody.do',textData).then((res) => {
+    const formData = new FormData();
+    if(fileData.value){
+        formData.append('file', fileData.value);
+    }
+    formData.append(
+        'text', 
+        new Blob([JSON.stringify(textData)],{type: 'application/json'}))
+
+    axios.post('/api/board/noticeFileSaveForm.do',formData).then((res) => {
         if(res.data.result === 'success'){
             modalState.setModalState();
             emit('postSuccess');
@@ -58,16 +80,34 @@ const searchDetail = () => {
     axios.post("/api/board/noticeDetailBody.do", { noticeSeq: props.idx })
     .then((res) => {
         noticeDetail.value = res.data.detail;
+        if(
+            noticeDetail.value.fileExt === 'jpg' || 
+            noticeDetail.value.fileExt === 'gif' || 
+            noticeDetail.value.fileExt === 'png' ||
+            noticeDetail.value.fileExt === 'webp'
+        ){
+            //미리보기를 위한 로직
+            getFileImage();
+        }
     });
 }
 
 const handlerUpdateBtn = () => {
     const textData = {
         title: noticeDetail.value.title, 
-        context: noticeDetail.value.content, 
+        content: noticeDetail.value.content, 
         noticeSeq: props.idx,
+    };
+
+    const formData = new FormData();
+    if(fileData.value){
+        formData.append('file', fileData.value);
     }
-    axios.post('/api/board/noticeUpdateBody.do',textData)
+    formData.append(
+        'text', 
+        new Blob([JSON.stringify(textData)],{type: 'application/json'}));
+
+    axios.post('/api/board/noticeUpdateFileForm.do',formData)
     .then((res) => {
         if(res.data.result === 'success'){
             modalState.setModalState();
@@ -85,6 +125,51 @@ const handlerDeleteBtn = () => {
         }
     })
 }
+
+const handlerFile = (e) =>{
+    const fileInfo = e.target.files;
+    const fileInfoSplit = fileInfo[0].name.split('.');
+    const fileExtension = fileInfoSplit[1].toLowerCase();
+    if(fileExtension === 'jpg' || fileExtension === 'gif' || fileExtension === 'png' || fileExtension === 'webp'){
+        imageUrl.value = URL.createObjectURL(fileInfo[0]);
+    }
+    fileData.value = fileInfo[0];
+}
+
+const getFileImage = () => {
+    let param = new URLSearchParams();
+    param.append('noticeSeq', props.idx);
+    const postAction = {
+        url: '/api/board/noticeDownload.do', 
+        method: 'POST',
+        data: param,
+        responseType: 'blob',
+    };
+    axios(postAction).then((res) => {
+        const url = window.URL.createObjectURL(new Blob([res.data]));
+        imageUrl.value = url;
+    }); 
+}
+
+const fileDownload = () => {
+    let param = new URLSearchParams();
+    param.append('noticeSeq', props.idx);
+    const postAction = {
+        url: '/api/board/noticeDownload.do', 
+        method: 'POST',
+        data: param,
+        responseType: 'blob',
+    };
+    axios(postAction).then((res) => {
+        const url = window.URL.createObjectURL(new Blob([res.data]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', noticeDetail.value.fileName);
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+    });
+};
 
 onMounted(() => {
     console.log(props.idx);
